17b97e2dc1efe387cc63b371654631748082347a
Hieu Nguyen <nxhieuqn@gmail.com>
2020-03-28 20:35:07 +1100

    changed app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          algdo1 <code>src/App.js</code> .
+          I will not give up <code>src/App.js</code> .
         </p>
         <a
           className="App-link"
